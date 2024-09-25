fef958e96e4bf8d81c8e07528f2b16aeb4b809d4
Balu <165539336+balu-z@users.noreply.github.com>
2024-09-25 23:57:34 +0530

    Add files via upload

---
+++ Task3/CustomException.java
@@ -0,0 +1,15 @@
+package exception;
+
+
+public class CustomException extends Exception
+{
+      public CustomException(){
+                super();
+       }
+
+      public CustomException(String message){
+                super(message);
+       }
+      
+      
+}
