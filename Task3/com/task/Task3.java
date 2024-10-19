5f1c16ac993d138cf5e1507fd66634dfc731edc6
Balu <165539336+balu-z@users.noreply.github.com>
2024-10-19 11:23:43 +0530

    Update Task3.java
    updated at 19/10/2024

--- Task3/com/task/Task3.java
+++ Task3/com/task/Task3.java
@@ -6,142 +6,267 @@ public class Task3
 {
 public  char[] toCharArray(String input) throws InputNullException
 {
-       Utility.checkNull(input);
-      return input.toCharArray();
+     try{
+               Utility.checkNull(input);
+               return input.toCharArray();
+         }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
 }
 public  char charAtSpecifiedIndex(String input,int index) throws InputNullException
 {
-     Utility.checkNull(input);
-     Utility.boundaryCheck(index);
-      return input.charAt(index);
+  try{
+        Utility.checkNull(input);
+        Utility.boundaryCheck(index);
+        return input.charAt(index);
+    }
+  catch(NullPointerException e)
+  {
+            throw new InputNullException("Error occured in  input :",e);
+   }
+  catch(Exception e)
+  {
+            throw new InputNullException("Error occured in  integer input :",e);
+   }
 }
 public  int charOccurence(String input,char ch) throws InputNullException
 {
-     int count=0;
-     int n=Utility.getLength(input);
-      for(int i=0;i<n;i++)
-      {
-             if(input.charAt(i)==ch)
+ try{
+            int count=0;
+            int n=Utility.getLength(input);
+             for(int i=0;i<n;i++)
              {
-                   count++;
-              }
-      }
-      return count;
+                    if(input.charAt(i)==ch)
+                    {
+                          count++;
+                     }
+             }
+             return count;
+   }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
  }
 
   public int greatestPosition(String input1,String input2) throws InputNullException
   {
-         Utility.checkNull(input1);
-      return input1.lastIndexOf(input2);
+     try{
+           Utility.checkNull(input1);
+           return input1.lastIndexOf(input2);
+        }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
    }
 
-  public String lastCharacters(String input,int noOfChar) throws InputNullException
-  {   
-        int n=Utility.getLength(input);
-       Utility.boundaryCheck(noOfChar,n);
+ public String lastCharacters(String input,int noOfChar) throws InputNullException
+ {   
+    try{
+         int n=Utility.getLength(input);
+         Utility.boundaryCheck(noOfChar,n);
          return input.substring(n-noOfChar);       
-}
-
+      }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
+      catch(Exception e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
+  }
  public String firstChar(String input,int noOfChar ) throws InputNullException
   { 
+     try{
             Utility.boundaryCheck(noOfChar,Utility.getLength(input));
-           return input.substring(0,noOfChar);  
+            return input.substring(0,noOfChar);  
+       }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
+      catch(Exception e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
  }
   public String replaceFirstChar(String input,int noOfChar,String repl) throws InputNullException
   {
+     try{
            Utility.checkNull(input);
            Utility.checkNull(repl);
            Utility.boundaryCheck(noOfChar);
            String replace =input.substring(0,noOfChar);
            return input.replaceFirst(replace,repl);
+         }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
+      catch(Exception e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
  public boolean isStartsWith(String input1,String input2)  throws InputNullException
   {
+      try{
             Utility.checkNull(input1);
             Utility.checkNull(input2);
-      return input1.startsWith(input2);
+            return input1.startsWith(input2);
+          }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
 
 public boolean isEndsWith(String input1,String input2)  throws InputNullException
   {
+      try{
             Utility.checkNull(input1);
             Utility.checkNull(input2);
-      return input1.endsWith(input2);
+            return input1.endsWith(input2);
+        }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
 
 public String toUppercase(String input) throws InputNullException
   {
+   try{
             Utility.checkNull(input);
-      return input.toUpperCase();
+            return input.toUpperCase();
+    }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+      }
   }
 
 public String toLowercase(String input) throws InputNullException
-  {
+  { 
+     try{
             Utility.checkNull(input);
-      return input.toLowerCase();
+            return input.toLowerCase();
+     }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+      }
   }
 
 public String stringReverse(String input) throws InputNullException
   {
-      Utility.checkNull(input);
-     char[]arr=input.toCharArray(); 
-      int i =arr.length-1,j=0;
-      while(i>j)
-       {
-             arr[i]=(char)((arr[i]+arr[j])-(arr[j]=arr[i]));
-             i--;
-             j++;
+    try{
+             Utility.checkNull(input);
+            char[]arr=input.toCharArray(); 
+             int i =arr.length-1,j=0;
+             while(i>j)
+              {
+                    arr[i]=(char)((arr[i]+arr[j])-(arr[j]=arr[i]));
+                    i--;
+                    j++;
+              }
+             return String.valueOf(arr);
+      }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
        }
-      return String.valueOf(arr);
   }
    
   public String lineWithMultipleStrings(String input) throws InputNullException
   {
+      try{
            Utility.checkNull(input);
-          return input;
+           return input;
+         }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
 
   public String lineWithMultipleStringsAndConcatenate(String input,String replaceWhat,String replaceWord) throws InputNullException
   {
+     try{
              Utility.checkNull(input);
-            Utility. checkNull(replaceWord);
+             Utility. checkNull(replaceWord);
              Utility.checkNull(replaceWhat);
-       return input.replace(replaceWhat,replaceWord);
+             return input.replace(replaceWhat,replaceWord);
+       }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
 
    public String[] toSplit(String input,String whereToSplit) throws InputNullException
    {
+       try{
              Utility.checkNull(input);
              Utility.checkNull(whereToSplit);
-              return input.split(whereToSplit);
+             return input.split(whereToSplit);
+         }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
    }
 
   public String toMergeString(String input,String []str) throws InputNullException
   { 
-            Utility.checkNull(input); 
-            if(str.length==0)
-              {
-                      return "String needed";
-              }
-           return String.join(input,str);
+        try{
+                   Utility.checkNull(input); 
+                   Utility.checkNull(str);
+                   return String.join(input,str);
+          }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
 }
   public boolean isEquals(String input1,String input2) throws InputNullException
   {
-           Utility.checkNull(input1);
+      try{
+            Utility. checkNull(input1);
             Utility.checkNull(input2);
-          return input1.equals(input2);
+            return input1.equals(input2);
+        }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }
 
   public boolean isEqualsIgnoreCase(String input1,String input2) throws InputNullException
   {
+     try{
             Utility.checkNull(input1);
             Utility.checkNull(input2);
-     return input1.equalsIgnoreCase(input2);
+            return input1.equalsIgnoreCase(input2);
+        }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
   }  
   public String toRemoveSpace(String input) throws InputNullException
   {
+       try{
            Utility.checkNull(input);
              return input.trim();
-   }
-     
+         }
+      catch(NullPointerException e)
+      {
+            throw new InputNullException("Error occured in  input :",e);
+       }
+   }     
 }
